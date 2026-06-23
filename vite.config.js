5b20fcab34d7d1694de54a8947cd5cd1b641ff6b
Zion <zion.lopes@sempreceub.com>
2026-06-23 17:27:07 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/nome-do-seu-repositorio/' // <--- ADICIONE ESTA LINHA EXATAMENTE ASSIM
+  base: '/tpizza/' // <--- ADICIONE ESTA LINHA EXATAMENTE ASSIM
 })
